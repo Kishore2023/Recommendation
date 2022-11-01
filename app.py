be494d81f5dbc58cbc2202035119e5d613b8f96a
Kishore2023 <114608753+Kishore2023@users.noreply.github.com>
2022-11-01 16:46:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,53 +4,53 @@ import pandas as pd
 
 st.title('Food Recommendation')
 
-breakfast = pd.read_excel("C:\\Users\\gan8k\\OneDrive - Contoso\\Documents\\Kishore - Personal\\Data Science\\Recommendation Engine Project\\Github1- Streamlit\\Dataset\\BreakfastVeg.xlsx")
-pickle.dump(breakfast, open('BreakfastVeg.pkl','wb'))
+#breakfast = pd.read_excel("C:\\Users\\gan8k\\OneDrive - Contoso\\Documents\\Kishore - Personal\\Data Science\\Recommendation Engine Project\\Github1- Streamlit\\Dataset\\BreakfastVeg.xlsx")
+#pickle.dump(breakfast, open('BreakfastVeg.pkl','wb'))
 breakfast_list = pickle.load(open('BreakfastVeg.pkl','rb'))
 Orderbreakfast = pd.DataFrame(breakfast_list)
 
 
-lunch = pd.read_excel("C:\\Users\\gan8k\\OneDrive - Contoso\\Documents\\Kishore - Personal\\Data Science\\Recommendation Engine Project\\Github1- Streamlit\\Dataset\\LunchVeg.xlsx")
-pickle.dump(lunch, open('LunchVeg.pkl','wb'))
+#lunch = pd.read_excel("C:\\Users\\gan8k\\OneDrive - Contoso\\Documents\\Kishore - Personal\\Data Science\\Recommendation Engine Project\\Github1- Streamlit\\Dataset\\LunchVeg.xlsx")
+#pickle.dump(lunch, open('LunchVeg.pkl','wb'))
 lunch_list = pickle.load(open('LunchVeg.pkl','rb'))
 Orderlunch = pd.DataFrame(lunch_list)
 
-dinner = pd.read_excel("C:\\Users\\gan8k\\OneDrive - Contoso\\Documents\\Kishore - Personal\\Data Science\\Recommendation Engine Project\\Github1- Streamlit\\Dataset\\DinnerVeg.xlsx")
-pickle.dump(dinner, open('DinnerVeg.pkl','wb'))
+#dinner = pd.read_excel("C:\\Users\\gan8k\\OneDrive - Contoso\\Documents\\Kishore - Personal\\Data Science\\Recommendation Engine Project\\Github1- Streamlit\\Dataset\\DinnerVeg.xlsx")
+#pickle.dump(dinner, open('DinnerVeg.pkl','wb'))
 dinner_list = pickle.load(open('DinnerVeg.pkl','rb'))
 Orderdinner = pd.DataFrame(dinner_list)
 
 #-------------------------------
 
-breakfastNV = pd.read_excel("C:\\Users\\gan8k\\OneDrive - Contoso\\Documents\\Kishore - Personal\\Data Science\\Recommendation Engine Project\\Github1- Streamlit\\Dataset\\BreakfastNV.xlsx")
-pickle.dump(breakfastNV, open('BreakfastNV.pkl','wb'))
+#breakfastNV = pd.read_excel("C:\\Users\\gan8k\\OneDrive - Contoso\\Documents\\Kishore - Personal\\Data Science\\Recommendation Engine Project\\Github1- Streamlit\\Dataset\\BreakfastNV.xlsx")
+#pickle.dump(breakfastNV, open('BreakfastNV.pkl','wb'))
 breakfastNV_list = pickle.load(open('BreakfastNV.pkl','rb'))
 OrderbreakfastNV = pd.DataFrame(breakfastNV_list)
 
-lunchNV = pd.read_excel("C:\\Users\\gan8k\\OneDrive - Contoso\\Documents\\Kishore - Personal\\Data Science\\Recommendation Engine Project\\Github1- Streamlit\\Dataset\\LunchNV.xlsx")
-pickle.dump(lunchNV, open('LunchNV.pkl','wb'))
+#lunchNV = pd.read_excel("C:\\Users\\gan8k\\OneDrive - Contoso\\Documents\\Kishore - Personal\\Data Science\\Recommendation Engine Project\\Github1- Streamlit\\Dataset\\LunchNV.xlsx")
+#pickle.dump(lunchNV, open('LunchNV.pkl','wb'))
 lunchNV_list = pickle.load(open('LunchNV.pkl','rb'))
 OrderlunchNV = pd.DataFrame(lunchNV_list)
 
-dinnerNV = pd.read_excel("C:\\Users\\gan8k\\OneDrive - Contoso\\Documents\\Kishore - Personal\\Data Science\\Recommendation Engine Project\\Github1- Streamlit\\Dataset\\DinnerNV.xlsx")
-pickle.dump(dinnerNV, open('DinnerNV.pkl','wb'))
+#dinnerNV = pd.read_excel("C:\\Users\\gan8k\\OneDrive - Contoso\\Documents\\Kishore - Personal\\Data Science\\Recommendation Engine Project\\Github1- Streamlit\\Dataset\\DinnerNV.xlsx")
+#pickle.dump(dinnerNV, open('DinnerNV.pkl','wb'))
 dinnerNV_list = pickle.load(open('DinnerNV.pkl','rb'))
 OrderdinnerNV = pd.DataFrame(dinnerNV_list)
 
 #------------------------------------------------------------------------
 
-breakfastBoth = pd.read_excel("C:\\Users\\gan8k\\OneDrive - Contoso\\Documents\\Kishore - Personal\\Data Science\\Recommendation Engine Project\\Github1- Streamlit\\Dataset\\BreakfastBoth.xlsx")
-pickle.dump(breakfastBoth, open('BreakfastBoth.pkl','wb'))
+#breakfastBoth = pd.read_excel("C:\\Users\\gan8k\\OneDrive - Contoso\\Documents\\Kishore - Personal\\Data Science\\Recommendation Engine Project\\Github1- Streamlit\\Dataset\\BreakfastBoth.xlsx")
+#pickle.dump(breakfastBoth, open('BreakfastBoth.pkl','wb'))
 breakfastBoth_list = pickle.load(open('BreakfastBoth.pkl','rb'))
 OrderbreakfastBoth = pd.DataFrame(breakfastBoth_list)
 
-lunchBoth = pd.read_excel("C:\\Users\\gan8k\\OneDrive - Contoso\\Documents\\Kishore - Personal\\Data Science\\Recommendation Engine Project\\Github1- Streamlit\\Dataset\\LunchBoth.xlsx")
-pickle.dump(lunchBoth, open('LunchBoth.pkl','wb'))
+#lunchBoth = pd.read_excel("C:\\Users\\gan8k\\OneDrive - Contoso\\Documents\\Kishore - Personal\\Data Science\\Recommendation Engine Project\\Github1- Streamlit\\Dataset\\LunchBoth.xlsx")
+#pickle.dump(lunchBoth, open('LunchBoth.pkl','wb'))
 lunchBoth_list = pickle.load(open('LunchBoth.pkl','rb'))
 OrderlunchBoth = pd.DataFrame(lunchBoth_list)
 
-dinnerBoth = pd.read_excel("C:\\Users\\gan8k\\OneDrive - Contoso\\Documents\\Kishore - Personal\\Data Science\\Recommendation Engine Project\\Github1- Streamlit\\Dataset\\DinnerBoth.xlsx")
-pickle.dump(dinnerBoth, open('DinnerBoth.pkl','wb'))
+#dinnerBoth = pd.read_excel("C:\\Users\\gan8k\\OneDrive - Contoso\\Documents\\Kishore - Personal\\Data Science\\Recommendation Engine Project\\Github1- Streamlit\\Dataset\\DinnerBoth.xlsx")
+#pickle.dump(dinnerBoth, open('DinnerBoth.pkl','wb'))
 dinnerBoth_list = pickle.load(open('DinnerBoth.pkl','rb'))
 OrderdinnerBoth = pd.DataFrame(dinnerBoth_list)
 
